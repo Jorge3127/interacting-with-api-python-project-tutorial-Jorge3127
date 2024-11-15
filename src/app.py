9d12f6809cf7ce90694605c070101f885a405d56
Jorge3127 <jorge.pedroza3127@gmail.com>
2024-11-15 22:32:22 +0000

    TERMINACION DEL EJERCICIO

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 import os
 import pandas as pd
 import seaborn as sns
+import spotipy
 from dotenv import load_dotenv
+
+CLIENT_ID="0e724116d5604b5a98695b83a31c0e2b"
+CLIENT_SECRET="568c83764e0d425d9da292ecac700de8"
+
 
 # load the .env file variables
 load_dotenv()
